f5e567755646f7778ef1300d06fb62cf765609b9
Silas João <silasroot321@gmail.com>
2022-05-28 11:10:08 -0300

    Update

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,18 +3,43 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
 from keras import Sequential
-from keras.layers import Dense, Flatten 
+from keras.layers import Dense, Flatten
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 
-def get_model(X_train, y_train, N, patience=10):
+def split_dataset_train(X_train, y_train, random):
+  """
+  Dividi as matrizes em subconjuntos aleatórios de treino e teste além de
+  realizar a padronização por meio de centralização e dimensionamento.
+
+  param random: coeficiente a ser adotado como embaralhamento dos dados antes da divisão
+  param X_train, y_train: conjunto de dados de treino 
+  
+  return: conjunto de dados de treino e validação aleatórios e padronizados.
+  """
+  
+  X_train, X_test, y_train, y_test = train_test_split(
+      X_train, y_train, test_size=0.2, random_state=random)
+  
+  scaler = StandardScaler()
+  
+  X_train_scaled = scaler.transform(X_train)
+  X_test_scaled = scaler.transform(X_test)
+
+  return X_test_scaled, X_train_scaled, y_train, y_test 
+
+
+def getModelMLP(X_train, y_train, N, batch_size, patience=10):
     """
     Cria uma pre-definição de um modelo.
 
     param X_train, y_train: conjunto de dados de treinamento.
     param X_test, y_test: conjunto de dados de teste.
     param patience: número de épocas sem melhoria.
-    param N: Número de amostras
+    param N: número de amostras
+    param batch_size: tamanho do lote a ser propagado na rede neural
 
     return model: modelo pre-definido. 
     """
@@ -25,7 +50,7 @@ def get_model(X_train, y_train, N, patience=10):
     #model.add(Dense(4, activation='relu'))
     model.add(Dense(4))
     model.compile(optimizer='adam', loss='mse')
-    model.fit(X_train, y_train, epochs=300, callbacks=[stop], validation_split=0.3, batch_size=32)
+    model.fit(X_train, y_train, epochs=300, callbacks=[stop], validation_split=0.3, batch_size=batch_size)
     
     return model
 
@@ -33,7 +58,7 @@ def get_model(X_train, y_train, N, patience=10):
 def least_squares(x, y):
   """
   Esta função usa o método dos mínimos quadrados como forma de estimar os coeficientes da reta de regressão
-  param x, y: diagrama de dispersão.
+  param x, y: dados de amostras.
 
   return b0, b1: coeficientes estimados.
   """
@@ -76,38 +101,36 @@ def plot_regression_line(x, y, b):
   plt.show()
 
 
-""" def heaviside(v_signal):
+# def heaviside(v_signal):
     
-    Função de ativação Heaviside 
-    :param v_signal : campo local induzido do neurônio.
+#     Função de ativação Heaviside 
+#     :param v_signal : campo local induzido do neurônio.
     
-    :return activFunction: retorna valor 0 ou 1 dependendo do campo local induzido.
+#     :return activFunction: retorna valor 0 ou 1 dependendo do campo local induzido.
 
-    activFunction = np.heaviside(v_signal, 1)
-    return activFunction
+#     activFunction = np.heaviside(v_signal, 1)
+#     return activFunction
 
-    # Forma alternativa
-    # return 1 * (v_signal >= 0) - 1 * (v_signal < 0)
-"""
+#     # Forma alternativa
+#     # return 1 * (v_signal >= 0) - 1 * (v_signal < 0)
 
-""" def sigmoid(v_signal, a):
+
+# def sigmoid(v_signal, a):
   
-  Função de ativação sigmoid 
-  :param v_signal : campo local induzido do neurônio.
-  :param a : parâmetro de inclinação da função sigmoid.
+#   Função de ativação sigmoid 
+#   :param v_signal : campo local induzido do neurônio.
+#   :param a : parâmetro de inclinação da função sigmoid.
 
-  :return activFunction: retorna uma faixa contínua de valores 0 e 1.
+#   :return activFunction: retorna uma faixa contínua de valores 0 e 1.
   
-  return 1 / (1 + np.exp(-a * v_signal))
- """
+#   return 1 / (1 + np.exp(-a * v_signal))
 
-""" def signum(v_signal):
+# def signum(v_signal):
     
-    Função de ativação signum
-    :param v_signal : campo local induzido do neurônio.
+#     Função de ativação signum
+#     :param v_signal : campo local induzido do neurônio.
 
-    :return activFunction: retorna valores na faixa de -1 e 1.
+#     :return activFunction: retorna valores na faixa de -1 e 1.
     
-    activFunction = np.sign(v_signal)
-    return activFunction
-"""
+#     activFunction = np.sign(v_signal)
+#     return activFunction
