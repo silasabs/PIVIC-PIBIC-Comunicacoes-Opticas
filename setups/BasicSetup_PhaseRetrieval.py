f439aefae5fc5f3199110780f35350edea007978
Silas João <silasroot321@gmail.com>
2023-01-08 12:43:00 -0300

    Update

--- setups/BasicSetup_PhaseRetrieval.py
+++ setups/BasicSetup_PhaseRetrieval.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import matplotlib.pyplot as plt
@@ -10,7 +10,7 @@ import numpy as np
 from commpy.modulation import QAMModem
 
 from optic.dsp import pulseShape, firFilter, decimate, symbolSync, resample
-from optic.models import phaseNoise, KramersKronigRx, linFiberCh
+from optic.models import phaseNoise, KramersKronigRx, linFiberCh, photodiode
 
 from optic.tx import simpleWDMTx
 from optic.core import parameters
@@ -20,11 +20,12 @@ from optic.metrics import fastBERcalc, monteCarloGMI, monteCarloMI, signal_power
 from optic.plot import pconst
 
 import scipy.constants as const
+from tqdm.notebook import tqdm
 
 
 # ### Simulation of a single polarization optical signal transmission
 
-# In[24]:
+# In[19]:
 
 
 ## Transmitter parameters:
@@ -35,14 +36,14 @@ paramTx.SpS = 4                # samples per symbol
 paramTx.Nbits = 400000         # total number of bits per polarization
 paramTx.pulse = "rrc"          # pulse shaping filter
 paramTx.Ntaps = 1024           # number of pulse shaping filter coefficients
-paramTx.alphaRRC = 0.9         # RRC rolloff
+paramTx.alphaRRC = 0.01        # RRC rolloff
 paramTx.Pch_dBm = 0            # power of the optical signal [dBm]
 paramTx.Nch = 1                # number of WDM channels
 paramTx.Fc = 193.1e12          # central frequency of the optical spectrum
 paramTx.freqSpac = 37.5e9      # WDM grid spacing
 
 ## Optical channel parameters:
-Ltotal = 50      # total link distance [km]
+Ltotal = 0      # total link distance [km]
 alpha = 0        # fiber loss parameter [dB/km]
 D = 16          # fiber dispersion parameter [ps/nm/km]
 Fc = paramTx.Fc  # central optical frequency of the WDM spectrum [Hz]
@@ -53,7 +54,7 @@ Fc = paramTx.Fc  # central optical frequency of the WDM spectrum [Hz]
 FO = 5e9 + paramTx.Rs/2  # frequency offset
 lw = 0*200e3       # linewidth
 ϕ_lo = 0           # initial phase in rad
-Plo_dBm = 12        # power in dBm
+Plo_dBm = 12       # power in dBm
 
 # ADC sampling rate
 paramADC = parameters()
@@ -66,10 +67,15 @@ chIndex = 0  # index of the channel to be demodulated
 plotPSD = True
 Fs = paramTx.Rs * paramTx.SpS  # simulation sampling rate
 
+# photodiode parameters
+paramPD = parameters()
+paramPD.B  = 1.1*paramTx.Rs
+paramPD.Fs = Fs
+
 
 # ### Core simulation code
 
-# In[25]:
+# In[20]:
 
 
 # generate optical signal signal
@@ -157,23 +163,25 @@ plt.title("optical WDM spectrum");
 
 # ### Phase-retrieval stage
 
-# In[26]:
+# In[21]:
 
+
+# simulate ideal direct-detection optical receiver
+Ipd = photodiode(sigRx, paramPD)
+Amp = np.sqrt(Ipd)
+Amp = resample(Amp, paramADC)
 
 # resampling to ADC sampling rate
 sigCh = resample(sigCh, paramADC)
 sfm = resample(sfm, paramADC)
 newFs = paramADC.SpS_out*paramTx.Rs
 
-# simulate ideal direct-detection optical receiver
-Amp = np.abs(sigRx)
-Amp = resample(Amp, paramADC)
-
 # Kramers-Kronig phase-retrieval
 phiTime = KramersKronigRx(Amp, newFs)
 
 # optical field reconstruction
 sigRx = Amp*np.exp(1j*phiTime)
+sfm_KK = sigRx.copy()
 
 # remove DC level
 sigRx -= np.sqrt(Plo)  # np.mean(sigRx)
@@ -230,7 +238,7 @@ print('SIR = ', round(10*np.log10(SIR),2), ' dB')
 
 # #### Standard receiver processing
 
-# In[27]:
+# In[22]:
 
 
 # resample to 2 samples/symbol:
@@ -269,7 +277,7 @@ sigRx = sigRx.reshape(-1, 1)
 symbRx = symbolSync(sigRx, symbTx, 2)
 
 
-# In[28]:
+# In[23]:
 
 
 # Power normalization
@@ -300,7 +308,7 @@ discard = int(paramEq.L[0]/2)
 pconst(y_EQ[discard:-discard, :], lim=True)
 
 
-# In[29]:
+# In[24]:
 
 
 # Carrier phase recovery
@@ -329,7 +337,7 @@ pconst(y_CPR[discard:-discard, :], lim=True)
 
 # #### Evaluate transmission metrics
 
-# In[30]:
+# In[25]:
 
 
 # correct for (possible) phase ambiguity
@@ -340,31 +348,390 @@ for k in range(y_CPR.shape[1]):
 y_CPR = y_CPR / np.sqrt(signal_power(y_CPR))
 
 ind = np.arange(discard, d.shape[0] - discard)
-BER, SER, SNR = fastBERcalc(y_CPR[ind, :], d[ind, :], mod)
-GMI, _ = monteCarloGMI(y_CPR[ind, :], d[ind, :], mod)
-MI = monteCarloMI(y_CPR[ind, :], d[ind, :], mod)
+BER, SER, SNR = fastBERcalc(y_CPR[ind, :], d[ind, :], paramTx.M, 'qam')
+GMI, _ = monteCarloGMI(y_CPR[ind, :], d[ind, :], paramTx.M, 'qam')
+MI = monteCarloMI(y_CPR[ind, :], d[ind, :], paramTx.M, 'qam')
 
 print("Results:\n")
 print("SER: %.2e" % (SER[0]))
 print("BER: %.2e" % (BER[0]))
 print("SNR: %.2f dB" % (SNR[0]))
 print("MI: %.2f bits" % (MI[0]))
-print("GMI: %.2f bits" % (GMI[0]))
+print("GMI: %.2f bits" % (GMI[0]));
 
 
-# In[2]:
+# In[13]:
 
 
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Conv1DTranspose, Conv1D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import EarlyStopping
 import tensorflow as tf
 from tensorflow.keras import backend as K
+from keras.utils.vis_utils import plot_model
 
 
-# ### Getting the neural network model for each Rolloff value acquired with a fixed FO (Frequency Offset).
+# ## Neural network training
 
-# In[3]:
+# In[30]:
+
+
+N = 256 # number of input amplitude samples to the NN
+
+sfm = sfm/np.sqrt(signal_power(sfm))
+
+sigPhase = np.angle(sfm) # get signal phase samples (labels) (L,)
+sigAmp   = np.pad(Amp, (int(N/2), int(N/2)), 'constant') # get signal amplitude samples (L,)
+
+# create set of input features
+X_train = np.zeros((len(sigPhase), N)) #(L,N)
+
+for indPhase in range(len(sigPhase)):
+    X_train[indPhase] = sigAmp[indPhase:N+indPhase]
+
+# create set of phase labels
+y_train = np.array([sfm.real, sfm.imag]).T
+
+# define neural network model
+stop = EarlyStopping(monitor='val_loss', patience=5)
+model = Sequential()
+model.add(BatchNormalization())
+model.add(Dense(4, input_shape=(N,)))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(2))
+model.compile(optimizer='adam', loss='mse')
+
+model.fit(X_train, y_train, epochs=100, callbacks=[stop],
+      validation_split=0.3, batch_size=256)
+
+model.summary()
+
+cw1 = model.layers[1].get_weights()
+plt.plot(cw1[0]);
+
+#model.save('NN_models/testModel_SpS_'+str(paramADC.SpS_out))
+
+
+# In[31]:
+
+
+sfm_NN = model.predict(X_train)
+
+
+# In[32]:
+
+
+sfm_KK = sfm_KK/np.sqrt(signal_power(sfm_KK))
+plt.figure(figsize=(9,4))
+plt.plot(sfm_NN[300:400,0],'g-o',label='NN')
+plt.plot(np.real(sfm_KK)[300:400],'b-s',label='KK')
+plt.plot(np.real(sfm)[300:400],'k-x',label='True')
+
+plt.plot(sfm_NN[300:400,1],'g--o',label='NN')
+plt.plot(np.imag(sfm_KK)[300:400],'b--s',label='KK')
+plt.plot(np.imag(sfm)[300:400],'k--x',label='True')
+plt.legend();
+
+
+# In[34]:
+
+
+N = np.array([32, 64, 128, 256, 512]) # number of input amplitude samples to the NN
+MSE = np.zeros(N.size)
+
+for indNN, indNN_ in enumerate(tqdm(N)):
+    
+    N = indNN_
+    sfm = sfm/np.sqrt(signal_power(sfm))
+
+    sigPhase = np.angle(sfm) # get signal phase samples (labels) (L,)
+    sigAmp   = np.pad(Amp, (int(N/2), int(N/2)), 'constant') # get signal amplitude samples (L,)
+
+    # create set of input features
+    X_train = np.zeros((len(sigPhase), N)) #(L,N)
+
+    for indPhase in range(len(sigPhase)):
+        X_train[indPhase] = sigAmp[indPhase:N+indPhase]
+
+    # create set of phase labels
+    y_train = np.array([sfm.real, sfm.imag]).T
+
+    # define neural network model
+    stop = EarlyStopping(monitor='val_loss', patience=5)
+    model = Sequential()
+    model.add(BatchNormalization())
+    model.add(Dense(4, input_shape=(N,)))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(4, activation='relu'))
+    model.add(Dense(2))
+    model.compile(optimizer='adam', loss='mse')
+
+    history = model.fit(X_train, y_train, epochs=100, callbacks=[stop], validation_split=0.3, batch_size=256)
+
+    #model.save('ConvNets_models/convnet_model_SpS_'+str(paramTx.SpS), save_format='h5')
+    mse_train  = np.mean(history.history['loss'])
+    MSE[indNN] = mse_train
+
+
+# In[38]:
+
+
+plt.figure(num=None, figsize=(10, 6))
+plt.plot(N, 20*np.log10(MSE), 'k--o', color='b', label='mlp training samples')
+plt.grid()
+plt.legend()
+plt.xlabel('Samples')
+plt.ylabel('MSE(dB)')
+plt.title('Sample Saturation Analysis')
+
+
+# In[40]:
+
+
+N = model.layers[1].get_weights()[0].shape[0]
+
+# generate optical signal signal
+sigTx, symbTx_, paramTx = simpleWDMTx(paramTx)
+
+# simulate linear signal propagation
+sigCh = linFiberCh(sigTx, Ltotal, alpha, D, Fc, Fs)
+
+# receiver detection and demodulation
+Fc = paramTx.Fc
+Ts = 1 / Fs
+mod = QAMModem(m=paramTx.M)
+
+freqGrid = paramTx.freqGrid
+print(
+    "Demodulating channel #%d , fc: %.4f THz, λ: %.4f nm\n"
+    % (
+        chIndex,
+        (Fc + freqGrid[chIndex]) / 1e12,
+        const.c / (Fc + freqGrid[chIndex]) / 1e-9,
+    )
+)
+
+symbTx = symbTx_[:, :, chIndex]
+
+Plo = 10 ** (Plo_dBm / 10) * 1e-3  # power in W
+
+print(
+    "Local oscillator P: %.2f dBm, lw: %.2f kHz, FO: %.2f MHz"
+    % (Plo_dBm, lw / 1e3, FO / 1e6)
+)
+
+# generate LO field
+π = np.pi
+t = np.arange(0, len(sigCh))*Ts
+ϕ_pn_lo = phaseNoise(lw, len(sigCh), Ts)
+
+sigLO = np.sqrt(Plo) * np.exp(-1j * (2 * π * FO * t + ϕ_lo + ϕ_pn_lo))
+
+# Add LO to the received signal
+sigRx = np.sqrt(Plo) + sigCh* np.exp(1j * (2 * π * FO * t + ϕ_lo + ϕ_pn_lo))
+sfm   = sigRx.copy()
+
+print('CSPR = %.2f dB'%(10*np.log10(signal_power(sigLO)/signal_power(sigCh))))
+
+# simulate ideal direct-detection optical receiver
+Ipd = photodiode(sigRx, paramPD)
+Amp = np.sqrt(Ipd)
+Amp = resample(Amp, paramADC)
+
+# resampling to ADC sampling rate
+sigCh = resample(sigCh, paramADC)
+sfm = resample(sfm, paramADC)
+newFs = paramADC.SpS_out*paramTx.Rs
+
+sfm = sfm/np.sqrt(signal_power(sfm))
+
+sigPhase = np.angle(sfm) # get signal phase samples (labels) (L,)
+sigAmp   = np.pad(Amp, (int(N/2), int(N/2)), 'constant') # get signal amplitude samples (L,)
+
+# create set of input features
+X_input = np.zeros((len(sigPhase), N)) #(L,N)
+
+for indPhase in range(len(sigPhase)):
+    X_input[indPhase] = sigAmp[indPhase:N+indPhase]
+
+sigRx_NN = model.predict(X_input)
+
+# optical field reconstruction
+sigRx = sigRx_NN[:,0]+1j*sigRx_NN[:,1]
+
+# remove DC level
+sigRx -= np.mean(sigRx) # np.sqrt(Plo)  
+
+# downshift to baseband
+t = np.arange(0, len(sigRx))*(1/newFs)
+sigRx *= np.exp(-1j * (2 * π * FO * t))
+
+# plot spectrum of  the reconstructed field
+plt.psd(
+    sigCh/np.sqrt(signal_power(sigCh)),
+    Fs=newFs,
+    Fc=paramTx.Fc,
+    NFFT=4 * 1024,
+    sides="twosided",
+    label="original signal spectrum",
+)
+
+plt.psd(
+    sigRx/np.sqrt(signal_power(sigRx)),
+    Fs=newFs,
+    Fc=paramTx.Fc,
+    NFFT=4 * 1024,
+    sides="twosided",
+    label=" signal spectrum after NN",
+)
+
+# correct for (possible) phase ambiguity
+rot = np.mean(sigCh/sigRx)
+sigRx = rot * sigRx
+sigRx = sigRx / np.sqrt(signal_power(sigRx))
+
+# plot spectrum of  the reconstructed field
+plt.psd(
+    sigRx/np.sqrt(signal_power(sigRx))-sigCh/np.sqrt(signal_power(sigCh)),
+    Fs=newFs,
+    Fc=paramTx.Fc,
+    NFFT=4 * 1024,
+    sides="twosided",
+    label="spectral difference",
+)
+plt.legend(loc="lower left")
+plt.title("optical WDM spectrum");
+
+intf = sigRx/np.sqrt(signal_power(sigRx))-sigCh/np.sqrt(signal_power(sigCh))
+
+SIR = 1/signal_power(intf)
+
+print('SIR = ', round(10*np.log10(SIR), 2), ' dB')
+
+
+# In[41]:
+
+
+# Matched filtering
+if paramTx.pulse == "nrz":
+    pulse = pulseShape("nrz", paramADC.SpS_out)
+elif paramTx.pulse == "rrc":
+    pulse = pulseShape(
+        "rrc", paramADC.SpS_out, N=paramTx.Ntaps, alpha=paramTx.alphaRRC, Ts=1 / paramTx.Rs
+    )
+
+pulse = pulse / np.max(np.abs(pulse))
+sigRx = firFilter(pulse, sigRx)
+
+# resample to 2 samples/symbol:
+paramRes = parameters()
+paramRes.Rs = paramTx.Rs
+paramRes.SpS_in  = paramADC.SpS_out
+paramRes.SpS_out = 2
+
+sigRx = resample(sigRx, paramRes)
+
+# plot constellations after matched filtering
+pconst(sigRx[0::paramRes.SpS_out], lim=True, R=3)
+
+# CD compensation
+sigRx = edc(sigRx, Ltotal, D, Fc, paramRes.SpS_out*paramTx.Rs)
+
+#plot constellations after CD compensation
+pconst(sigRx[0::paramRes.SpS_out], lim=True, R=2)
+
+# Downsampling to 2 sps and re-synchronization with transmitted sequences
+sigRx = sigRx.reshape(-1, 1)
+
+symbRx = symbolSync(sigRx, symbTx, 2)
+
+# Power normalization
+x = sigRx
+d = symbRx
+
+x = x.reshape(len(x), 1) / np.sqrt(signal_power(x))
+d = d.reshape(len(d), 1) / np.sqrt(signal_power(d))
+
+# Adaptive equalization
+mod = QAMModem(m=paramTx.M)
+
+paramEq = parameters()
+paramEq.nTaps = 15
+paramEq.SpS = 2
+paramEq.mu = [5e-3, 2e-3]
+paramEq.numIter = 5
+paramEq.storeCoeff = False
+paramEq.alg = ["nlms", "dd-lms"]
+paramEq.M = paramTx.M
+paramEq.L = [20000, 80000]
+
+y_EQ, H, errSq, Hiter = mimoAdaptEqualizer(x, dx=d, paramEq=paramEq)
+
+discard = int(paramEq.L[0]/2)
+
+#plot constellations after adaptive equalization
+pconst(y_EQ[discard:-discard,:], lim=True)
+
+
+# In[42]:
+
+
+# Carrier phase recovery
+paramCPR = parameters()
+paramCPR.alg = "bps"
+paramCPR.M = paramTx.M
+paramCPR.N = 65
+paramCPR.B = 64
+paramCPR.pilotInd = np.arange(0, len(y_EQ), 20)
+
+y_CPR, θ = cpr(y_EQ, symbTx=d, paramCPR=paramCPR)
+
+y_CPR = y_CPR / np.sqrt(signal_power(y_CPR))
+
+plt.figure()
+plt.title("CPR estimated phase")
+plt.plot(θ, "-")
+plt.xlim(0, len(θ))
+plt.grid()
+
+discard = 5000
+
+# plot constellations after CPR
+pconst(y_CPR[discard:-discard, :], lim=True)
+
+
+# In[43]:
+
+
+# correct for (possible) phase ambiguity
+for k in range(y_CPR.shape[1]):
+    rot = np.mean(d[:, k] / y_CPR[:, k])
+    y_CPR[:, k] = rot * y_CPR[:, k]
+
+y_CPR = y_CPR / np.sqrt(signal_power(y_CPR))
+
+ind = np.arange(discard, d.shape[0] - discard)
+BER, SER, SNR = fastBERcalc(y_CPR[ind, :], d[ind, :], paramTx.M, 'qam')
+GMI, _ = monteCarloGMI(y_CPR[ind, :], d[ind, :],  paramTx.M, 'qam')
+MI = monteCarloMI(y_CPR[ind, :], d[ind, :],  paramTx.M, 'qam')
+
+print("Results:\n")
+print("SER: %.2e" % (SER[0]))
+print("BER: %.2e" % (BER[0]))
+print("SNR: %.2f dB" % (SNR[0]))
+print("MI: %.2f bits" % (MI[0]))
+print("GMI: %.2f bits" % (GMI[0]));
+
+
+# In[44]:
+
+
+cw1 = model.layers[1].get_weights()
+plt.plot(cw1[0],'-o');
+
+
+# In[45]:
 
 
 ## Transmitter parameters:
@@ -382,10 +749,10 @@ paramTx.Fc = 193.1e12          # central frequency of the optical spectrum
 paramTx.freqSpac = 37.5e9      # WDM grid spacing
 
 ## Optical channel parameters:
-Ltotal = 50      # total link distance [km]
-alpha = 0        # fiber loss parameter [dB/km]
-D = 16          # fiber dispersion parameter [ps/nm/km]
-Fc = paramTx.Fc  # central optical frequency of the WDM spectrum [Hz]
+Ltotal = 100      # total link distance [km]
+alpha = 0         # fiber loss parameter [dB/km]
+D = 16            # fiber dispersion parameter [ps/nm/km]
+Fc = paramTx.Fc   # central optical frequency of the WDM spectrum [Hz]
 
 ## Receiver parameters:
 
@@ -406,17 +773,23 @@ chIndex = 0  # index of the channel to be demodulated
 plotPSD = True
 Fs = paramTx.Rs * paramTx.SpS  # simulation sampling rate
 
+# photodiode parameters
+paramPD = parameters()
+paramPD.B  = 1.1*paramTx.Rs
+paramPD.Fs = Fs
 
-# In[4]:
+
+# In[50]:
 
 
+path = 'C:/Users/optic/Documents/PIVIC-PIBIC-Comunicacoes-Opticas/setups/NN_models/'
 N = 256  # number of input amplitude samples to the NN
 
-Rolloff = np.arange(0.01, 0.99, 0.1)
-FO_Values = np.arange(1e9, 6e9, 1e9)
+Rolloff = np.arange(0.05, 0.95, 0.05)
+FO_Values = np.arange(0e9, 6e9, 1e9)
 
-for indexFO, indFO_ in enumerate(FO_Values):
-    for indNN, indNN_ in enumerate(Rolloff):
+for indexFO, indFO_ in enumerate(tqdm(FO_Values)):
+    for indNN, indNN_ in enumerate(tqdm(Rolloff)):
     
         # Main simulation parameters
         paramTx.alphaRRC = indNN_
@@ -469,19 +842,20 @@ for indexFO, indFO_ in enumerate(FO_Values):
 
         # In[4]:
 
+        # simulate ideal direct-detection optical receiver
+        Ipd = photodiode(sigRx, paramPD)
+        Amp = np.sqrt(Ipd)
+        Amp = resample(Amp, paramADC)
+
         # resampling to ADC sampling rate
         sigCh = resample(sigCh, paramADC)
         sfm = resample(sfm, paramADC)
         newFs = paramADC.SpS_out*paramTx.Rs
 
-        # simulate ideal direct-detection optical receiver
-        Amp = np.abs(sigRx)
-        Amp = resample(Amp, paramADC)
-
         sfm = sfm/np.sqrt(signal_power(sfm))
         
         sigPhase = np.angle(sfm)  # get signal phase samples (labels) (L,)
-        sigAmp = np.pad(np.abs(sfm), (int(N/2), int(N/2)), 'constant') # get signal amplitude samples (L,)
+        sigAmp = np.pad(Amp, (int(N/2), int(N/2)), 'constant') # get signal amplitude samples (L,)
 
         # create set of input features
         X_train = np.zeros((len(sigPhase), N))  # (L,N)
@@ -490,50 +864,22 @@ for indexFO, indFO_ in enumerate(FO_Values):
         for indPhase in range(len(sigPhase)):
             X_train[indPhase] = sigAmp[indPhase:N+indPhase]
 
+        # create set of phase labels
+        y_train = np.array([sfm.real, sfm.imag]).T
+
         # define neural network model
         stop = EarlyStopping(monitor='val_loss', patience=5)
         model = Sequential()
         model.add(BatchNormalization())
         model.add(Dense(4, input_shape=(N,)))
         model.add(Dense(8, activation='relu'))
-        # model.add(Dense(8,activation='gelu'))
         model.add(Dense(4, activation='relu'))
-        model.add(Dense(1))
+        model.add(Dense(2))
         model.compile(optimizer='adam', loss='mse')
 
-        model.fit(X_train, y_train, epochs=100, callbacks=[stop],  validation_split=0.3, batch_size=256)
+        model.fit(X_train, y_train, epochs=100, callbacks=[stop],
+            validation_split=0.3, batch_size=256)
 
-        model.summary()
-
-        #cw1 = model.layers[1].get_weights()
-        #plt.plot(cw1[0])
-        
-        # Save Model
-        model.save('NN_models/testModel_SpS_'+str(paramADC.SpS_out)+'_FO_'+str(FO/10e9)+'GHz_Rolloff_'+str(round(paramTx.alphaRRC, 2)))
-
-
-# In[34]:
-
-
-model = tf.keras.models.load_model('NN_models/testModel_SpS_'+str(paramADC.SpS_out)+'_FO_2.1GHz_Rolloff_0.91')
-
-phiTime_NN = model.predict(X_train)
-phiTime_NN = phiTime_NN.reshape(-1,)
-
-
-# In[35]:
-
-
-plt.figure(figsize=(9,4))
-plt.plot(np.angle(sfm)[300:400],'-o',label='True')
-plt.plot(phiTime[300:400],'x',label='KK')
-plt.plot(phiTime_NN[300:400],'s',label='NN');
-plt.legend();
-
-
-# In[33]:
-
-
-cw1 = model.layers[1].get_weights()
-plt.plot(cw1[0],'-o');
+        #model.summary()
+        model.save(path+'testModel_SpS_'+str(paramADC.SpS_out)+'_FO_'+str(FO/10e9)+'GHz_Rolloff_'+str(round(paramTx.alphaRRC, 2)), save_format='h5')
 
